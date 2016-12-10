a2b88c8851daed128c25d160963f19276a88be0c
Manuel <m.moracuesta@gmail.com>
2016-12-10 15:48:48 +0100

    TNT objects, Input controller for create, modify and destroy tnts.

---
+++ Assets/BuildPlane.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildPlane : MonoBehaviour {
+    public GameObject prefab;
+    public int maxTNTs = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnMouseDown()
+    {
+        Ray pos = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(pos.origin,pos.direction,Color.red,100);
+        RaycastHit info;
+        if (Physics.Raycast(pos,out info,1000))
+        {
+            if (TNT.totalTNTs < maxTNTs)
+            {
+                GameObject obj = Instantiate<GameObject>(prefab,info.point,Quaternion.identity);
+            }
+
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/TNT/MouseActionsController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseActionsController : MonoBehaviour 
+{
+    public enum MouseAction
+    {
+        HOLD,
+        CLICK,
+        DRAG_UP,
+        DRAG_DOWN,
+    }
+    MouseAction m_action;
+    public delegate void ActionDelegate(MouseAction actionType);
+    public ActionDelegate onFinishAction;
+    private float m_acumTime;
+    private Vector3 m_prevMousePosition;
+    public float m_minTimeToDrag = 0.5f;
+    public float m_distanceToDetectDrag = 1;
+    public int m_timeToDetectHold = 1;
+
+    public void OnMouseDown()
+    {
+        m_action = MouseAction.CLICK;
+        m_acumTime = 0;
+        m_prevMousePosition = Input.mousePosition;
+    }
+
+    public void OnMouseUp()
+    {
+        if (m_action == MouseAction.CLICK)
+            onFinishAction.Invoke(m_action);
+    }
+
+    public void OnMouseDrag()
+    {
+        m_acumTime += Time.deltaTime;
+        if (CheckDrag())
+        {
+            onFinishAction(m_action);
+        }
+        else
+        {
+            if (m_acumTime > m_timeToDetectHold && m_action != MouseAction.DRAG_UP && m_action != MouseAction.DRAG_DOWN)
+            {
+                m_action = MouseAction.HOLD;
+                onFinishAction(m_action);
+            }
+        }
+    }
+
+    private bool CheckDrag()
+    {
+        Debug.Log(Input.mousePosition.y - m_prevMousePosition.y);
+        bool toReturn = false;
+        if (m_acumTime < m_minTimeToDrag) return toReturn;
+
+
+        if (Input.mousePosition.y - m_prevMousePosition.y > m_distanceToDetectDrag)
+        {
+            m_action = MouseAction.DRAG_UP;
+            m_prevMousePosition = Input.mousePosition;
+            toReturn = true;
+        }
+        else if (Input.mousePosition.y - m_prevMousePosition.y < -m_distanceToDetectDrag)
+        {
+            m_action = MouseAction.DRAG_DOWN;
+            m_prevMousePosition = Input.mousePosition;
+            toReturn = true;
+        }
+        return toReturn;
+    }
+
+
+
+}
