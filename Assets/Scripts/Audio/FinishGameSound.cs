845c39450935990fae302f143b098584e641aa6d
Chiki <chiki4089@gmail.com+>
2016-12-10 20:20:02 +0100

    - Victory sound is called for when the sound exists.

---
+++ Assets/Scripts/Audio/FinishGameSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishGameSound : MonoBehaviour {
+
+	public void PlayGameOver() {
+		UtilSound.instance.PlaySound("gameover");
+	}
+
+	public void PlayVictory() {
+		UtilSound.instance.PlaySound("victory");
+	}
+}
