e70920696acbd87d392ad14a154099120e90868c
Manuel <m.moracuesta@gmail.com>
2016-12-10 16:45:47 +0100

    Added explosions object in main scene

---
+++ Assets/Scripts/UI/Bilboard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bilboard : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        transform.forward = -Camera.main.transform.forward;
+        transform.up = Camera.main.transform.up;
+	}
+}
