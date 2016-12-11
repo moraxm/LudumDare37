2398ea8624b51491945834ded45c6397f1a16e09
Manuel <m.moracuesta@gmail.com>
2016-12-11 11:27:32 +0100

    Quit game button

---
+++ Assets/Scripts/UI/OnlyWindows.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyWindows : MonoBehaviour 
+{
+    public void Awake()
+    {
+#if !UNITY_STANDALONE
+        gameObject.SetActive(false);
+#endif
+    }
+}
