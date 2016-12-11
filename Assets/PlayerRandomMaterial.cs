390a200c6c083a912105b1c4c358ed67df57a9cb
Chiki <chiki4089@gmail.com+>
2016-12-11 11:28:10 +0100

    - Player selects random material on Awake. - Adjusted TNT collider.

---
+++ Assets/PlayerRandomMaterial.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRandomMaterial : MonoBehaviour {
+
+	public Material[] materials;
+
+	private void Awake() {
+		if (materials.Length > 0) {
+			int num = Random.Range(0, materials.Length);
+			gameObject.GetComponent<Renderer>().material = materials[num];
+		}
+	}
+}
